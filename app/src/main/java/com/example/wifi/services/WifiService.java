7edb1942f04aac6cc75aeefefbeeadcc9b4d8ed2
chebaby <ech.chebaby@gmail.com>
2019-11-18 00:44:29 +0100

    [wip]

--- app/src/main/java/com/example/wifi/services/WifiService.java
+++ app/src/main/java/com/example/wifi/services/WifiService.java
@@ -1,12 +1,19 @@
 package com.example.wifi.services;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.util.Log;
 
-import com.example.wifi.enums.ServiceAction;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.wifi.R;
 import com.example.wifi.enums.ServiceState;
 
 public class WifiService extends Service {
@@ -16,6 +23,9 @@ public class WifiService extends Service {
     private PowerManager.WakeLock wakeLock;
 
     private com.example.wifi.utils.Service ServiceUtils;
+
+    private static final int NOTIFICATION_ID = 1;
+    private static final String PRIMARY_CHANNEL = "default";
 
 
 
@@ -36,10 +46,10 @@ public class WifiService extends Service {
             Log.d(TAG, "\n\nonStartCommand using an intent with action " + action);
 
             switch (action) {
-                case ServiceAction.START.name():
+                case "START":
                     this.startTheService();
                     break;
-                case ServiceAction.STOP.name():
+                case "STOP":
                     this.stopTheService();
                     break;
                 default:
@@ -73,8 +83,8 @@ public class WifiService extends Service {
         super.onCreate();
 
         Log.i(TAG, "\n\nonCreate : The service has been created");
-        val notification = createNotification();
-        startForeground(1, notification);
+
+        startForeground(NOTIFICATION_ID, createNotification(this));
     }
 
 
@@ -85,7 +95,17 @@ public class WifiService extends Service {
         super.onDestroy();
 
         Log.i(TAG, "\n\nonDestroy: The service has been destroyed");
+
+        stopForeground(true);
+
+        if (wakeLock != null && wakeLock.isHeld()) {
+            wakeLock.release();
+        }
+//        if (trackingController != null) {
+//            trackingController.stop();
+//        }
     }
+
 
 
 
@@ -93,127 +113,82 @@ public class WifiService extends Service {
 
         if (isServiceStarted) return;
 
+        startForeground(NOTIFICATION_ID, createNotification(this));
+
         Log.i(TAG, "\n\nstartTheService: Starting the foreground service task");
 
         isServiceStarted = true;
 
+        // TODO: 11/17/2019 probably i sould name "ServiceUtils" something like "PersistServiceState" or "StoreServiceState"
         ServiceUtils.setState(this, ServiceState.STARTED);
 
         // we need this lock so our service gets not affected by Doze Mode
-        wakeLock =
-                (getSystemService(Context.POWER_SERVICE) as PowerManager).run {
-            newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "EndlessService::lock").apply {
-                acquire()
-            }
-        }
+        PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, getClass().getName());
+        wakeLock.acquire();
 
-        if (SerialPortService.WAKELOCK == null) {
-            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-            SerialPortService.WAKELOCK = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, SerialPortService.WL_TAG);
-            SerialPortService.WAKELOCK.acquire();
-            startService(new Intent(getApplicationContext(), SerialPortService.class));
-        }
+//        trackingController = new TrackingController(this);
+//        trackingController.start();
 
-        // we're starting a loop in a coroutine
-        GlobalScope.launch(Dispatchers.IO) {
-            while (isServiceStarted) {
-                launch(Dispatchers.IO) {
-                    pingFakeServer()
-                }
-                delay(1 * 60 * 1000)
-            }
-            log("End of the loop for the service")
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            ContextCompat.startForegroundService(this, new Intent(this, HideNotificationService.class));
         }
     }
 
 
 
+    public void stopTheService() {
 
-    private fun stopService() {
-        log("Stopping the foreground service")
-        Toast.makeText(this, "Service stopping", Toast.LENGTH_SHORT).show()
-        try {
-            wakeLock?.let {
-                if (it.isHeld) {
-                    it.release()
-                }
-            }
-            stopForeground(true)
-            stopSelf()
-        } catch (e: Exception) {
-            log("Service stopped without being started: ${e.message}")
+        Log.i(TAG, "\n\nstopTheService: Stopping the foreground service");
+
+        stopForeground(true);
+
+        if (wakeLock != null && wakeLock.isHeld()) {
+            wakeLock.release();
         }
-        isServiceStarted = false
-        setServiceState(this, ServiceState.STOPPED)
+
+//        if (trackingController != null) {
+//            trackingController.stop();
+//        }
+
+        isServiceStarted = false;
+        ServiceUtils.setState(this, ServiceState.STOPPED);
     }
 
-    private fun pingFakeServer() {
-        val df = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.mmmZ")
-        val gmtTime = df.format(Date())
 
-        val deviceId = Settings.Secure.getString(applicationContext.contentResolver, Settings.Secure.ANDROID_ID)
 
-        val json =
-                """
-        {
-            "deviceId": "$deviceId",
-                "createdAt": "$gmtTime"
-        }
-        """
-        try {
-            Fuel.post("https://jsonplaceholder.typicode.com/posts")
-                    .jsonBody(json)
-                    .response { _, _, result ->
-                    val (bytes, error) = result
-                if (bytes != null) {
-                    log("[response bytes] ${String(bytes)}")
-                } else {
-                    log("[response error] ${error?.message}")
-                }
-            }
-        } catch (e: Exception) {
-            log("Error making the request: ${e.message}")
-        }
+
+    private static Notification createNotification(Context context) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL)
+                .setSmallIcon(R.drawable.ic_stat_notify)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setCategory(NotificationCompat.CATEGORY_SERVICE);
+
+        Intent intent = new Intent(android.provider.Settings.ACTION_SETTINGS);
+
+        builder.setContentIntent(PendingIntent.getActivity(context, 0, intent, 0));
+        return builder.build();
     }
 
-    private fun createNotification(): Notification {
-        val notificationChannelId = "ENDLESS SERVICE CHANNEL"
 
-        // depending on the Android API that we're dealing with we will have
-        // to use a specific method to create the notification
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            val channel = NotificationChannel(
-                    notificationChannelId,
-                    "Endless Service notifications channel",
-                    NotificationManager.IMPORTANCE_HIGH
-            ).let {
-                it.description = "Endless Service channel"
-                it.enableLights(true)
-                it.lightColor = Color.RED
-                it.enableVibration(true)
-                it.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
-                it
-            }
-            notificationManager.createNotificationChannel(channel)
+
+
+    public static class HideNotificationService extends Service {
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
         }
 
-        val pendingIntent: PendingIntent = Intent(this, MainActivity::class.java).let { notificationIntent ->
-                PendingIntent.getActivity(this, 0, notificationIntent, 0)
+        @Override
+        public void onCreate() {
+            startForeground(NOTIFICATION_ID, createNotification(this));
+            stopForeground(true);
         }
 
-        val builder: Notification.Builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) Notification.Builder(
-                this,
-                notificationChannelId
-        ) else Notification.Builder(this)
-
-        return builder
-                .setContentTitle("Endless Service")
-                .setContentText("This is your favorite endless service working")
-                .setContentIntent(pendingIntent)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setTicker("Ticker text")
-                .setPriority(Notification.PRIORITY_HIGH) // for under android 26 compatibility
-                .build()
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            stopSelfResult(startId);
+            return START_NOT_STICKY;
+        }
     }
 }
